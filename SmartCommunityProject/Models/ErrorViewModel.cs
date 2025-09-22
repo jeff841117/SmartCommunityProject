bdeb31d72ef615447a5f67b82ce93fe0d7e2ce3c
Jeff <jeff841117@gmail.com>
2025-09-22 16:54:38 +0800

    base project

---
+++ SmartCommunityProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartCommunityProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
